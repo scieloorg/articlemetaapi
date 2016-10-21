e1051c4106a1b2a01c61321f6db9614b7f3d5965
fabiobatalha <fabiobatalha@gmail.com>
2016-10-21 16:05:55 -0200

    Ajustes em config

--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@
 from setuptools import setup, find_packages
 
 install_requires = [
-    'thriftpy=>0.3.1',
-    'requests=>2.8.1',
+    'thriftpy>=0.3.1',
+    'requests>=2.8.1',
     'xylose'
 ]
 
@@ -27,7 +27,7 @@ setup(
         "Programming Language :: Python :: 2.7",
     ],
     dependency_links=[
-        "git+https://git@github.com/scieloorg/xylose.git@v1.15.5#egg=xylose"
+        "git+https://git@github.com/scieloorg/xylose.git@v1.16.5#egg=xylose"
     ],
     tests_require=tests_require,
     test_suite='tests',
