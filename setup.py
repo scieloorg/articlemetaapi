159a3328ed27ecc3e22e915e4832549a8e42d563
fabiobatalha <fabiobatalha@gmail.com>
2017-11-13 13:38:32 -0200

    Atualizando versão de dependencias

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ from setuptools import setup, find_packages
 install_requires = [
     'thriftpy==0.3.1',
     'requests>=2.11.1',
-    'xylose>=1.30.0'
+    'xylose>=1.31.0'
 ]
 
 tests_require = []
 
 setup(
     name="articlemetaapi",
-    version="1.23.0",
+    version="1.24.0",
     description="Library that implements the endpoints of the ArticleMeta API",
     author="SciELO",
     author_email="scielo-dev@googlegroups.com",
