7e16c3a2387f81961f662354521d0761f57d8d36
Gustavo Fonseca <gustavo@gfonseca.net>
2018-07-23 15:48:26 -0300

    Melhora o setup.py
    As melhorias foram motivadas pela constatação de que várias das nossas
    aplicações contam com um pacote `tests` no toplevel do projeto e esse
    pacote está sendo distribuído, causando conflitos.

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
-from setuptools import setup, find_packages
+from setuptools import setup
 
 install_requires = [
-    'thriftpy==0.3.9',
+    'thriftpy>=0.3.9',
     'requests>=2.19.1',
     'xylose>=1.33.1'
 ]
@@ -11,22 +11,22 @@ tests_require = []
 
 setup(
     name="articlemetaapi",
-    version="1.26.2",
+    version="1.26.3",
     description="Library that implements the endpoints of the ArticleMeta API",
     author="SciELO",
     author_email="scielo-dev@googlegroups.com",
     maintainer="Fabio Batalha",
     maintainer_email="fabio.batalha@scielo.org",
-    url="http://github.com/scieloorg/processing",
-    packages=find_packages(),
+    url="http://github.com/scieloorg/articlemetaapi",
+    packages=['articlemeta'],
     include_package_data=True,
     classifiers=[
         "Development Status :: 4 - Beta",
         "Intended Audience :: Developers",
         "Programming Language :: Python :: 3",
     ],
-    dependency_links=[],
     tests_require=tests_require,
     test_suite='tests',
-    install_requires=install_requires
+    install_requires=install_requires,
+    zip_safe=False
 )
