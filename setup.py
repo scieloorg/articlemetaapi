ef8199778f649f02b2a0f9ebf791d17fb998ca48
Gustavo Fonseca <gustavo@gfonseca.net>
2018-10-16 14:26:39 -0300

    Ajusta os classificadores e o build do pacote

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-from setuptools import setup
+import setuptools
 
 install_requires = [
     'thriftpy>=0.3.9',
@@ -9,21 +9,26 @@ install_requires = [
 
 tests_require = []
 
-setup(
+setuptools.setup(
     name="articlemetaapi",
     version="1.26.4",
-    description="Library that implements the endpoints of the ArticleMeta API",
+    description="SciELO ArticleMeta SDK for Python",
     author="SciELO",
     author_email="scielo-dev@googlegroups.com",
-    maintainer="Fabio Batalha",
+    maintainer="Gustavo Fonseca",
     maintainer_email="fabio.batalha@scielo.org",
     url="http://github.com/scieloorg/articlemetaapi",
-    packages=['articlemeta'],
+    packages=setuptools.find_packages(
+        exclude=["*.tests", "*.tests.*", "tests.*", "tests"]),
     include_package_data=True,
     classifiers=[
-        "Development Status :: 4 - Beta",
+        "Development Status :: 5 - Production/Stable",
         "Intended Audience :: Developers",
-        "Programming Language :: Python :: 3",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
     ],
     tests_require=tests_require,
     test_suite='tests',
